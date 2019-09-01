c27c3c28178ad8e8222130023484ea3528032d25
Abu-Maestro <engryusufmayowa@gmail.com>
2019-09-01 17:25:12 +0100

    Returning Hello World on a page

--- runserver.py
+++ runserver.py
@@ -2,13 +2,26 @@
 This script runs the FlaskWebProject1 application using a development server.
 """
 
-from os import environ
-from FlaskWebProject1 import app
+#from os import environ
+#from FlaskWebProject1 import app
+
+#if __name__ == '__main__':
+ #   HOST = environ.get('SERVER_HOST', 'localhost')
+  #  try:
+    #     PORT = int(environ.get('SERVER_PORT', '5555'))
+  #  except ValueError:
+   #     PORT = 5555
+   # app.run(HOST, PORT)
+
+import os
+from HelloFlask import app    # Imports the code from HelloFlask/__init__.py
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+
     app.run(HOST, PORT)
